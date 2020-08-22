5f8adbff54eb143ddfac9af313aab395cebeb22e
procopio420 <lucas-procopio@outlook.com>
2020-08-22 19:48:48 -0300

    refactoring/styles

--- scripts/products.mjs
+++ scripts/products.mjs
@@ -3,7 +3,7 @@
 let ProductPageURL = 'https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1';
 
 const generateProductCard = ({ name, image, description, installments, oldPrice, price }) => {
-  return `<img src="${image}" alt="" class="product-img" />
+  return `<img src="${image}" alt="" class="w-100" />
   <h3 class="product-name">${name}</h3>
   <p class="product-description">${description}</p>
   <p class="old-price">De: R$${oldPrice.toFixed(2)}</p>
@@ -11,7 +11,7 @@ const generateProductCard = ({ name, image, description, installments, oldPrice,
   <p class="installment-payment">
   ou ${installments.count}x de R$${installments.value.toFixed(2)}
   </p>
-  <button class="button button-card">Comprar</button>`;
+  <button class="button w-100">Comprar</button>`;
 };
 
 const renderProductCard = (product) => {
